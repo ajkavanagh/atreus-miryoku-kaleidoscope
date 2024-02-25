6a268b47d17b953df96854648f83d36e1eb34573
Alex Kavanagh <alex@ajkavanagh.co.uk>
2024-02-25 12:00:05 +0000

    Improve formatting on keymaps
    Basically, make it easier to look at, so that it's possible to see how
    the keys will be laid out.

--- Atreus.ino
+++ Atreus.ino
@@ -37,6 +37,8 @@
 
 #define SL(n) ShiftToLayer(n)
 #define LL(n) LockLayer(n)
+// ML is used in key_defs/keymaps.h
+#define MTL(n) MoveToLayer(n)
 
 enum {
   MACRO_PRIMARY,
@@ -81,30 +83,30 @@ KEYMAPS(
 #if defined (PRIMARY_KEYMAP_QWERTY)
   [PRIMARY] = KEYMAP_STACKED
   (
-       Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
-      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+       Key_Q         ,Key_W     ,Key_E       ,Key_R         ,Key_T
+      ,Key_A         ,Key_S     ,Key_D       ,Key_F         ,Key_G
+      ,Key_Z         ,Key_X     ,Key_C       ,Key_V         ,Key_B         ,Key_Backtick
+      ,Key_Esc       ,Key_Tab   ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
 
-                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
-                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
-       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt  ,Key_Space ,SL(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+                     ,Key_Y     ,Key_U       ,Key_I         ,Key_O         ,Key_P
+                     ,Key_H     ,Key_J       ,Key_K         ,Key_L         ,Key_Semicolon
+      ,Key_Backslash ,Key_N     ,Key_M       ,Key_Comma     ,Key_Period    ,Key_Slash
+      ,Key_LeftAlt   ,Key_Space ,SL(FUN)     ,Key_Minus     ,Key_Quote     ,Key_Enter
   ),
 
 #elif defined (PRIMARY_KEYMAP_COLEMAK)
 
   [PRIMARY] = KEYMAP_STACKED
   (
-       Key_Q   ,Key_W   ,Key_F       ,Key_P         ,Key_B
-      ,Key_A   ,Key_R   ,Key_S       ,Key_T         ,Key_G
-      ,Key_Z   ,Key_X   ,Key_C       ,Key_D         ,Key_V, Key_Backtick
-      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+       Key_Q         ,Key_W     ,Key_F       ,Key_P         ,Key_B
+      ,Key_A         ,Key_R     ,Key_S       ,Key_T         ,Key_G
+      ,Key_Z         ,Key_X     ,Key_C       ,Key_D         ,Key_V         ,Key_Backtick
+      ,Key_Esc       ,Key_Tab   ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
 
-                     ,Key_J     ,Key_L      ,Key_U     ,Key_Y      ,Key_Semicolon
-                     ,Key_M     ,Key_N      ,Key_E     ,Key_I      ,Key_O
-       ,Key_Backslash,Key_K     ,Key_H      ,Key_Comma ,Key_Period ,Key_Slash
-       ,Key_LeftAlt  ,Key_Space ,SL(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+                     ,Key_J     ,Key_L       ,Key_U         ,Key_Y         ,Key_Semicolon
+                     ,Key_M     ,Key_N       ,Key_E         ,Key_I         ,Key_O
+      ,Key_Backslash ,Key_K     ,Key_H       ,Key_Comma     ,Key_Period    ,Key_Slash
+      ,Key_LeftAlt   ,Key_Space ,SL(FUN)     ,Key_Minus     ,Key_Quote     ,Key_Enter
   ),
 
 
@@ -113,30 +115,31 @@ KEYMAPS(
 #error "No default keymap defined. You should make sure that you have a line like '#define PRIMARY_KEYMAP_QWERTY' in your sketch"
 
 #endif
+
   [FUN] = KEYMAP_STACKED
   (
-       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
-      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
-      ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
-      ,LL(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete         ,Key_LeftControl
+       Key_Exclamation      ,Key_At                   ,Key_UpArrow    ,Key_Dollar           ,Key_Percent
+      ,Key_LeftParen        ,Key_LeftArrow            ,Key_DownArrow  ,Key_RightArrow       ,Key_RightParen
+      ,Key_LeftBracket      ,Key_RightBracket         ,Key_Hash       ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
+      ,LL(UPPER)            ,Key_Insert               ,Key_LeftGui    ,Key_LeftShift        ,Key_Delete            ,Key_LeftControl
 
-                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
-                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
-      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
-      ,Key_LeftAlt ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
+                            ,Key_PageUp               ,Key_7          ,Key_8                ,Key_9                 ,Key_Backspace
+                            ,Key_PageDown             ,Key_4          ,Key_5                ,Key_6                 ,___
+      ,Key_And              ,Key_Star                 ,Key_1          ,Key_2                ,Key_3                 ,Key_Plus
+      ,Key_LeftAlt          ,Key_Space                ,___            ,Key_Period           ,Key_0                 ,Key_Equals
    ),
 
   [UPPER] = KEYMAP_STACKED
   (
-       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
-      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
-      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
-      ,MoveToLayer(PRIMARY)   ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End              ,Key_PageUp
+      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow       ,Key_PageDown
+      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX                  ,___                   ,___
+      ,MTL(PRIMARY)          ,Consumer_VolumeDecrement ,___           ,___                  ,___                   ,___
 
-                ,Key_UpArrow   ,Key_F7              ,Key_F8          ,Key_F9         ,Key_F10
-                ,Key_DownArrow ,Key_F4              ,Key_F5          ,Key_F6         ,Key_F11
-      ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
-      ,___      ,___           ,MoveToLayer(PRIMARY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
+                             ,Key_UpArrow              ,Key_F7        ,Key_F8               ,Key_F9                ,Key_F10
+                             ,Key_DownArrow            ,Key_F4        ,Key_F5               ,Key_F6                ,Key_F11
+      ,___                   ,XXX                      ,Key_F1        ,Key_F2               ,Key_F3                ,Key_F12
+      ,___                   ,___                      ,MTL(PRIMARY)  ,Key_PrintScreen      ,Key_ScrollLock        ,Consumer_PlaySlashPause
    )
 )
 // clang-format on
